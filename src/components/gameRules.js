687a77fe8a62c7554df8a75d5881e0b1cccf290f
R-Cole <randy@goodglue.com>
2019-02-14 15:14:15 -0700

    basic setup

--- src/components/gameRules.js
+++ src/components/gameRules.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import GameRulesList from './gameRulesList';
 
 export class GameRules extends React.Component {
     componentDidMount() {
     }    
 
     render() {
+
+    let testSelectedGame = {
+            rules:[{description:'this is a rule...'},{description:'this is a another rule...'},{description:'this is yet another rule...'},{description:'this is, yes, another awesome rule...'}],
+          };  
+
       return (
         <div className="gameRules">
-      
+        <h3>Game Rules!</h3>
+        <GameRulesList selectedGameRules={testSelectedGame}/>
         </div>
         );
     }
@@ -16,6 +23,7 @@ export class GameRules extends React.Component {
 
 const mapStateToProps = state => {
     return {
+        games: state.auth.currentUser.games//<--in progress...
     };
 };
 
---
+++ src/components/gameRulesList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../App.css';
+
+export default function GameRulesList(props) {
+
+    let gameRuleItem = props.selectedGameRules.map((gameRule,index) => {
+        
+        //* could add select css here?
+
+        return(
+       
+          <li key={index}>
+            <p>Rule #{index + 1}: &nbsp; {gameRule}</p>
+          </li>
+    
+          );
+ 
+    });
+
+    
+
+    return (
+      <div>
+        <ul className="SideBar-lists" id='myGamesList'>
+          {gameRuleItem}
+        </ul>
+      </div>
+    )
+
+};
